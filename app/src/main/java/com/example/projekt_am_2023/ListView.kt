0cfc9f796ef11313419b5cc7404ebdde217f4db0
LokiNaBoki <jedrzejkowski@protonmail.com>
2023-04-27 23:51:42 +0200

    (ListView) add empty activity and set up run configuration to open it

---
+++ app/src/main/java/com/example/projekt_am_2023/ListView.kt
@@ -0,0 +1,11 @@
+package com.example.projekt_am_2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+    }
+}
