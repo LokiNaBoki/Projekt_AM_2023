e055b736970103d5dfc00ed89fece777372e1b8b
LokiNaBoki <jedrzejkowski@protonmail.com>
2023-04-29 18:34:42 +0200

    (TaskView) fill view with task data from intent

--- app/src/main/java/com/example/projekt_am_2023/TaskView.kt
+++ app/src/main/java/com/example/projekt_am_2023/TaskView.kt
@@ -5,15 +5,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class TaskView : AppCompatActivity() {
+    inner class SubtaskListener(private  val task: Task) : View.OnClickListener, View.OnLongClickListener {
+        override fun onClick(view: View?) {
+            Toast.makeText(this@TaskView, "onClick ${task.title}", Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onLongClick(view: View?): Boolean {
+            Toast.makeText(this@TaskView, "onLongClick ${task.title}", Toast.LENGTH_SHORT).show()
+            return false
+        }
+    }
+
     inner class NewSubtaskListener() : View.OnClickListener {
         override fun onClick(view: View?) {
             Toast.makeText(this@TaskView, "onClick New Subtask", Toast.LENGTH_SHORT).show()
@@ -26,7 +41,43 @@ class TaskView : AppCompatActivity() {
 
         val tagRecycler = findViewById<RecyclerView>(R.id.tagsRecycler)
         tagRecycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        tagRecycler.adapter = TagAdapter(emptyList())
+
+        val task = intent.getSerializableExtra("task")
+
+        if(task != null) {
+            val task = task as Task
+            findViewById<CheckBox>(R.id.doneCheckbox).isChecked = task.done
+            findViewById<TextView>(R.id.taskTitle).text = task.title
+
+            val drawableId: Int = task.assignee?.avatar ?: R.drawable.user_default
+            val drawable = ContextCompat.getDrawable(this, drawableId)
+            findViewById<ImageView>(R.id.userImage).setImageDrawable(drawable)
+            findViewById<TextView>(R.id.userText).text = task.assignee!!.name
+
+            findViewById<TextView>(R.id.startDate).text = task.getStartDate()
+            findViewById<TextView>(R.id.startTime).text = task.getStartTime()
+
+            findViewById<TextView>(R.id.endDate).text = task.getEndDate()
+            findViewById<TextView>(R.id.endTime).text = task.getEndTime()
+
+            findViewById<EditText>(R.id.descriptionText).setText(task.Description)
+
+            tagRecycler.adapter = TagAdapter(task.tags)
+
+            val subtasks = findViewById<LinearLayout>(R.id.subtasksRecycler)
+            for(subtask in task.subtasks) {
+                val tmp = LayoutInflater.from(this).inflate(R.layout.subtask, null, false)
+                tmp.findViewById<CheckBox>(R.id.doneCheckbox).isChecked = subtask.done
+                tmp.findViewById<TextView>(R.id.subtaskTitle).text = subtask.title
+
+                val listener = SubtaskListener(subtask)
+                tmp.setOnClickListener(listener)
+                tmp.setOnLongClickListener(listener)
+                subtasks.addView(tmp)
+            }
+        } else {
+            tagRecycler.adapter = TagAdapter(emptyList())
+        }
 
         val subtasks = findViewById<LinearLayout>(R.id.subtasksRecycler)
         val tmp = LayoutInflater.from(this).inflate(R.layout.new_element, null, false)
