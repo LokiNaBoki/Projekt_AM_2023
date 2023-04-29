43ad24f12068eca4207417c8739767cd5636a50f
LokiNaBoki <jedrzejkowski@protonmail.com>
2023-04-29 18:28:32 +0200

    (TaskView) add TaskView layout

---
+++ app/src/main/java/com/example/projekt_am_2023/TaskView.kt
@@ -0,0 +1,11 @@
+package com.example.projekt_am_2023
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TaskView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_view)
+    }
+}
