f5cc5605cc542238fd8d93e0b55ca5be0edb2e89
mysliwy112 <112mysliwy112@wp.pl>
2023-05-16 15:43:34 +0200

    (Database-Firebase) Add Firebase database. Add saving functions

--- app/src/main/java/com/example/projekt_am_2023/User.kt
+++ app/src/main/java/com/example/projekt_am_2023/User.kt
@@ -2,4 +2,8 @@ package com.example.projekt_am_2023
 
 import java.io.Serializable
 
-data class User(var name: String, var avatar: Int?) : Serializable
+data class User(
+    var name: String="",
+    var avatar: Int?=null,
+    var databaseId: String?=null
+) : Serializable
