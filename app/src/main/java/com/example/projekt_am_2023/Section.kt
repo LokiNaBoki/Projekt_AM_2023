762259b2e310ef1fc247fcc1fd1391365478c30f
mysliwy112 <112mysliwy112@wp.pl>
2023-05-19 22:34:53 +0200

    (Database-Firebase) Add loading of objects from database. Rework of saving in progress.

--- app/src/main/java/com/example/projekt_am_2023/Section.kt
+++ app/src/main/java/com/example/projekt_am_2023/Section.kt
@@ -1,9 +1,58 @@
 package com.example.projekt_am_2023
 
-import com.google.firebase.database.IgnoreExtraProperties
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
 
 data class Section(
     var name: String = "",
     var tasks: MutableList<Task> = mutableListOf(),
     var databaseId: String? = null
-    )
+    ){
+
+    fun saveDatabase(){
+        var key : String?
+        if(this.databaseId == null){
+            key = DataViewModel.sections.push().key
+        }else{
+            key = this.databaseId
+        }
+
+        if (key == null) {
+            Log.w("Firebase", "Couldn't get push key for posts")
+            return
+        }
+
+        var tasks = hashMapOf<String, Boolean>()
+        for(t in this.tasks){
+            if(t.databaseId == null){
+                Log.w("Firebase","Subtask not in database.")
+                return
+            }
+            tasks[t.databaseId!!] = true
+        }
+
+        val postValues = hashMapOf<String, Any>(
+            "name" to this.name,
+            "tasks" to tasks
+        )
+
+        val childUpdates = hashMapOf<String, Any>(
+            "/sections/$key" to postValues
+        )
+        this.databaseId = key
+        DataViewModel.dataref.updateChildren(childUpdates)
+    }
+
+    companion object{
+        fun loadDatabase(dataSnapshot: DataSnapshot) : Section{
+            var section : Section =  Section()
+            section.databaseId = dataSnapshot.key
+            section.name = dataSnapshot.child("name").value as String
+            section.tasks = Task.loadDatabaseArray(dataSnapshot.child("tasks"))
+//            for(t in dataSnapshot.child("subtasks").children){
+//                Task.loadDatabase(DataViewModel.tasks.child(t.key!!).get())
+//            }
+            return section
+        }
+    }
+}
--- app/src/main/java/com/example/projekt_am_2023/Tag.kt
+++ app/src/main/java/com/example/projekt_am_2023/Tag.kt
@@ -1,9 +1,55 @@
 package com.example.projekt_am_2023
 
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
 import java.io.Serializable
+import java.util.Calendar
 
 data class Tag(
     var name: String="",
     var color: Int=0,
+    var tasks: MutableList<Task> = mutableListOf(),
     var databaseId: String?=null
-) : Serializable
+) : Serializable{
+    fun saveDatabase(){
+        var key : String?
+        if(this.databaseId == null){
+            key = DataViewModel.sections.push().key
+        }else{
+            key = this.databaseId
+        }
+
+        if (key == null) {
+            Log.w("Firebase", "Couldn't get push key for posts")
+            return
+        }
+
+
+        val postValues = hashMapOf<String, Any>(
+            "name" to this.name,
+            "color" to this.color,
+
+            )
+
+        val childUpdates = hashMapOf<String, Any>(
+            "/tags/$key" to postValues
+        )
+        this.databaseId = key
+        DataViewModel.dataref.updateChildren(childUpdates)
+    }
+
+    companion object{
+        fun loadDatabaseArray(dataSnapshot: DataSnapshot) : MutableList<Tag>{
+            var tags = mutableListOf<Tag>()
+            tags.add(loadDatabase(dataSnapshot))
+            return tags
+        }
+        fun loadDatabase(dataSnapshot: DataSnapshot) : Tag{
+            var tag : Tag = Tag()
+            tag.databaseId = dataSnapshot.key
+            tag.name = dataSnapshot.child("name").value as String
+            tag.color = dataSnapshot.child("name").value as Int
+            return tag
+        }
+    }
+}
