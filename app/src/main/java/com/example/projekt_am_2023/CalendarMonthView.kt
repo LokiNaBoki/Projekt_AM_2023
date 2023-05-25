30489de8db8c2b348ab4e7371b52e0515fb7d236
Radek <radoslaw.dubieniecki@gmail.com>
2023-05-26 00:30:30 +0200

    (CalendarMonthView) change activity to fragment

---
+++ app/src/main/java/com/example/projekt_am_2023/CalendarMonthView.kt
@@ -0,0 +1,47 @@
+package com.example.projekt_am_2023
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CalendarView
+import androidx.fragment.app.Fragment
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+class CalendarMonthView : Fragment() {
+    private lateinit var fragment: ListView
+
+    inner class ChooseDateListener() : CalendarView.OnDateChangeListener {
+        override fun onSelectedDayChange(view: CalendarView, year: Int, month: Int, day: Int) {
+            //findViewById<TextView>(R.id.dateView).text = "$day $month"
+            val sdf = SimpleDateFormat("yyyy-MM-Dd HH:mm", Locale.getDefault())
+            val calendar: Calendar= Calendar.getInstance()
+            calendar.set(year, month, day)
+            fragment.changeDate(calendar)
+            //Toast.makeText(this@CalendarMonthViewActivity, "onDayChange $day", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.activity_calendar_month_view, container, false)
+        val chooseDateListener = ChooseDateListener()
+        view.findViewById<CalendarView>(R.id.calendarView).setOnDateChangeListener(chooseDateListener)
+        fragment = ListView.newInstance()
+        requireActivity().supportFragmentManager.beginTransaction().replace(R.id.childFragmentContainer, fragment).commit()
+        return view
+    }
+
+    companion object {
+        fun newInstance(): CalendarMonthView {
+            return CalendarMonthView()
+        }
+    }
+
+}
