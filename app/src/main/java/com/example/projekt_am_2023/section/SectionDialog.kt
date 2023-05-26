c1ce5dff6fad0eca6b53b20f823286faebeeb2d9
LokiNaBoki <jedrzejkowski@protonmail.com>
2023-05-26 19:10:38 +0200

    (SectionListFragment) add a fragment for displaying list of available sections

--- app/src/main/java/com/example/projekt_am_2023/section/SectionDialog.kt
+++ app/src/main/java/com/example/projekt_am_2023/section/SectionDialog.kt
@@ -19,15 +19,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projekt_am_2023.database.DatabaseLoader
 import com.example.projekt_am_2023.R
+import com.example.projekt_am_2023.tag.TagListFragment
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import java.io.Serializable
 
-class SectionDialog : DialogFragment() {
-    private var sections: MutableList<Section> = mutableListOf()
+class SectionDialog : DialogFragment(), SectionListFragment.SectionListListener {
     private lateinit var listener: SectionDialogListener
-    private lateinit var sectionAdapter: SectionAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,17 +35,6 @@ class SectionDialog : DialogFragment() {
         } else {
             parentFragment as SectionDialogListener
         }
-
-        DatabaseLoader.sections.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                sections = Section.loadDatabaseArray(dataSnapshot)
-                sectionAdapter.notifyDataSetChanged()
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                Log.w("Firebase", "sections - loadPost:onCancelled", databaseError.toException())
-            }
-        })
     }
 
     override fun onCreateView(
@@ -55,11 +43,8 @@ class SectionDialog : DialogFragment() {
     ): View? {
         val view = inflater.inflate(R.layout.dialog_section, container, false)
 
-        sectionAdapter = SectionAdapter()
-        view.findViewById<RecyclerView>(R.id.sectionRecycler).apply {
-            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-            adapter = sectionAdapter
-        }
+        val fragment = SectionListFragment.newInstance()
+        childFragmentManager.beginTransaction().replace(R.id.sectionFragment, fragment).commit()
 
         view.findViewById<Button>(R.id.cancel).setOnClickListener { dismiss() }
 
@@ -106,37 +91,8 @@ class SectionDialog : DialogFragment() {
         fun onFinishSectionDialog(section: Section)
     }
 
-    inner class SectionAdapter() : RecyclerView.Adapter<SectionAdapter.ViewHolder>() {
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
-            val text: TextView
-            init {
-                text = view as TextView
-                text.setOnClickListener(this)
-            }
-
-            override fun onClick(view: View?) {
-                listener.onFinishSectionDialog(sections[adapterPosition]);
-                dismiss();
-            }
-        }
-
-        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-            return ViewHolder(TextView(viewGroup.context).apply {
-                layoutParams = ViewGroup.MarginLayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT
-                ).apply {
-                    updateMargins(bottom = (10 * resources.displayMetrics.density).toInt())
-                }
-                textSize = 22f
-                setTextColor(Color.BLACK)
-            })
-        }
-
-        override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-            viewHolder.text.text = sections[position].name
-        }
-
-        override fun getItemCount() = sections.size
+    override fun onSectionSelected(section: Section) {
+        listener.onFinishSectionDialog(section)
+        dismiss()
     }
 }
---
+++ app/src/main/java/com/example/projekt_am_2023/section/SectionListFragment.kt
@@ -0,0 +1,105 @@
+package com.example.projekt_am_2023.section
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.updateMargins
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projekt_am_2023.R
+import com.example.projekt_am_2023.database.DatabaseLoader
+import com.example.projekt_am_2023.tag.TagListFragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import java.io.Serializable
+
+class SectionListFragment : Fragment() {
+    private var sections: MutableList<Section> = mutableListOf()
+    private lateinit var listener: SectionListListener
+    private lateinit var sectionAdapter: SectionAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        listener = if(parentFragment == null) {
+            requireContext() as SectionListListener
+        } else {
+            parentFragment as SectionListListener
+        }
+
+        DatabaseLoader.sections.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                sections = Section.loadDatabaseArray(dataSnapshot)
+                sectionAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("Firebase", "sections - loadPost:onCancelled", databaseError.toException())
+            }
+        })
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_section_list, container, false)
+
+        sectionAdapter = SectionAdapter()
+        view.findViewById<RecyclerView>(R.id.sectionRecycler).apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = sectionAdapter
+        }
+
+        return view
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = SectionListFragment().apply {
+            arguments = Bundle().apply { }
+        }
+    }
+
+    interface SectionListListener : Serializable {
+        fun onSectionSelected(section: Section)
+    }
+
+    inner class SectionAdapter() : RecyclerView.Adapter<SectionAdapter.ViewHolder>() {
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+            val text: TextView
+            init {
+                text = view as TextView
+                text.setOnClickListener(this)
+            }
+
+            override fun onClick(view: View?) {
+                listener.onSectionSelected(sections[adapterPosition]);
+            }
+        }
+
+        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(TextView(viewGroup.context).apply {
+                layoutParams = ViewGroup.MarginLayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                ).apply {
+                    updateMargins(bottom = (10 * resources.displayMetrics.density).toInt())
+                }
+                textSize = 22f
+                setTextColor(Color.BLACK)
+            })
+        }
+
+        override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+            viewHolder.text.text = sections[position].name
+        }
+
+        override fun getItemCount() = sections.size
+    }
+}
