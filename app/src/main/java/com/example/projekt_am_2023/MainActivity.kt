2182e2b677f570668fec404a2163ca766348055c
LokiNaBoki <jedrzejkowski@protonmail.com>
2023-05-26 18:57:12 +0200

    (MainView) add page listing all tags

--- app/src/main/java/com/example/projekt_am_2023/MainActivity.kt
+++ app/src/main/java/com/example/projekt_am_2023/MainActivity.kt
@@ -8,10 +8,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.example.projekt_am_2023.tag.EditTag
+import com.example.projekt_am_2023.tag.Tag
+import com.example.projekt_am_2023.tag.TagListFragment
 import com.example.projekt_am_2023.task.AddTask
 import com.google.android.material.tabs.TabLayoutMediator
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TagListFragment.TagListListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,17 +29,20 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = CustomStateAdapter(this).apply {
             addFragment(listFragment)
             addFragment(monthFragment)
+            addFragment(TagListFragment.newInstance())
         }
 
         TabLayoutMediator(findViewById(R.id.tabLayout), viewPager) { tab, position ->
             tab.setIcon(when(position) {
                 0 -> { R.drawable.list_icon }
                 1 -> { R.drawable.month_icon }
+                2 -> { R.drawable.section_icon }
                 else -> { R.drawable.none_icon }
             })
             tab.text = getString(when(position) {
                 0 -> { R.string.listLabel }
                 1 -> { R.string.monthLabel }
+                2 -> { R.string.sectionsLabel }
                 else -> { R.string.noneLabel }
             })
         }.attach()
@@ -52,6 +58,12 @@ class MainActivity : AppCompatActivity() {
 
     fun onNewClick(ignoredView: View) {
         startActivity(Intent(this, AddTask::class.java))
+    }
+
+    override fun onTagSelected(tag: Tag) {
+        val i = Intent(this, EditTag::class.java)
+        i.putExtra("tag", tag)
+        startActivity(i)
     }
 }
 
